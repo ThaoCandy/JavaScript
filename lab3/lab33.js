4bbe4897d8dc908648969098b2e8891edd05b93f
ThaoCandy <thaontph46896@fpt.edu.vn>
2023-11-12 14:02:39 +0700

    Nâng cấp lab33

--- lab3/lab33.js
+++ lab3/lab33.js
@@ -3,15 +3,24 @@
 var ketQua = document.getElementById("contain");
             var baoLoi = document.getElementById("error");
             var KQ;
+            var kq_cu = 0;
 
             function toanHang(a){
+                if(kq_cu != 0){
+                    return;
+                }
                 ketQua.innerText += a;
                 baoLoi.innerText = "";
             }
 
             function toanTu(o){
+                if(kq_cu != 0){
+                    ketQua.innerText = kq_cu;
+                    kq_cu = 0;
+                }
                 ketQua.innerText += o;
             }
+            
             function thucHien(){
                 if(ketQua.innerText == ""){
                     return;
@@ -25,6 +34,7 @@ var ketQua = document.getElementById("contain");
                     }
                 })
 
+                
 
                 try {
                     KQ = eval(contain_text);
@@ -33,11 +43,12 @@ var ketQua = document.getElementById("contain");
                     baoLoi.innerText = "Lỗi ! Hãy làm lại từ đầu";
                     return;
                 }
-                
+                kq_cu = KQ;
                 ketQua.innerText += '=' + KQ;
             }
 
             function lamLai(){
                 ketQua.innerText = "";
                 baoLoi.innerText = "";
+                kq_cu = 0;
             }
